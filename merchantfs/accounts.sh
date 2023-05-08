79c7918207a1332ff9d8c0861683ef6b8fe474df
Andrew Seif <aseif@kashier.io>
2023-05-08 04:56:13 +0300

    created staging branch

--- merchantfs/accounts.sh
+++ merchantfs/accounts.sh
@@ -1,8 +1,8 @@
 
 #Creating SA & Acces for merchant-portal
 
-oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-production:merchant-portal-production
+oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-staging:merchant-portal-staging
 
 #Creating SA & Acces for storybook
 
-oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-production:storybook-production
+oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-staging:storybook-staging
