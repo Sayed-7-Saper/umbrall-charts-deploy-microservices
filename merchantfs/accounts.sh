384bd02df2216fb48cf220d7e62bc38e5c66efb7
Andrew Seif <aseif@kashier.io>
2023-05-21 12:33:10 +0300

    fixed SA

--- merchantfs/accounts.sh
+++ merchantfs/accounts.sh
@@ -1,8 +1,8 @@
 
 #Creating SA & Acces for merchant-portal
 
-oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-development:merchant-portal-development
+oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-development:merchantfs-development-merchant-portal
 
 #Creating SA & Acces for storybook
 
-oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-development:storybook-development
+oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-development:merchantfs-development-storybook
