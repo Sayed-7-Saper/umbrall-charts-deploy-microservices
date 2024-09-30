5848e1f94235641b5363cc2493d2abe002fa2926
Elsayed Saber <esaber@kashier.io>
2024-09-30 15:13:25 +0000

    update

--- merchantfs/accounts.sh
+++
@@ -1,8 +0,0 @@
-
-#Creating SA & Acces for portal
-
-oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-development:merchantfs-development-portal
-
-#Creating SA & Acces for storybook
-
-oc adm policy add-scc-to-user anyuid system:serviceaccount:merchantfs-development:merchantfs-development-storybook
